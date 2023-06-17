713c468fd28d53001768446fabe4b21ae8bd44eb
Leandro Franco <leandrofranco91@gmail.com>
2023-06-17 17:02:14 -0300

    Initial scaffolding

---
+++ src/test/java/com/tenpo/jarvis/JarvisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenpo.jarvis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
